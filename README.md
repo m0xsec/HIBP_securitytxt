a9b6bf7d0ec7e6b8aec0b5802422f761725a6a98
m0x <m0x@m0x.io>
2023-07-23 18:01:17 -0700

    parse all breach models for every HIBP breach domain

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # HIBP security.txt
 
+Checks for the presence of a security.txt under the current list of breach domains for Have I Been Pwned.
+
 smol project idea proposed one weekend by the HIBP creator, [Troy Hunt](https://twitter.com/troyhunt/status/1682982538409828354).
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,51 @@
-fn main() {
+use reqwest::Client;
+use serde::Deserialize;
+
+/// HIBP Breach Model
+#[allow(dead_code)]
+#[derive(Deserialize)]
+#[serde(rename_all = "PascalCase")]
+struct Breach {
+    name: String,
+    title: String,
+    domain: String,
+    breach_date: String,
+    added_date: String,
+    modified_date: String,
+    pwn_count: i32,
+    description: String,
+    logo_path: String,
+    data_classes: Vec<String>,
+    is_verified: bool,
+    is_fabricated: bool,
+    is_sensitive: bool,
+    is_retired: bool,
+    is_spam_list: bool,
+}
+
+#[tokio::main]
+async fn main() {
     println!("Hello, world!");
+
+    // Pull the breach model for all breach domains from HIBP
+    let client = Client::new();
+    let url = "https://haveibeenpwned.com/api/v3/breaches";
+
+    let resp = client
+        .get(url)
+        .header("User-Agent", "HIBP_securitytxt")
+        .send();
+
+    let breaches: Vec<Breach> = resp.await.unwrap().json().await.unwrap();
+
+    // Pull all of the domains from the breach models
+    for breach in breaches {
+        if !breach.domain.is_empty() {
+            println!("Domain: {}", breach.domain);
+        }
+    }
+
+    // TODO: Kick off requests to check security.txt files for each domain
+    // Check domain.com/security.txt and domain.com/.well-known/security.txt
+    // Any other paths to check?
 }
